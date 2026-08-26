2bd6089ff2fa8f0f17cf51d7759b8188e20573f1
saxoji <saxoji@gmail.com>
2026-08-26 18:02:06 +0900

    Fix Flowise 746 Chat Message Route (#6719)
    fix(flowise-746): chat message route

--- packages/server/src/routes/chat-messages/index.ts
+++ packages/server/src/routes/chat-messages/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import chatMessageController from '../../controllers/chat-messages'
+import { checkAnyPermission } from '../../enterprise/rbac/PermissionCheck'
 const router = express.Router()
 
 // CREATE
@@ -7,12 +8,21 @@ const router = express.Router()
 // router.post(['/', '/:id'], chatMessageController.createChatMessage)
 
 // READ
-router.get(['/', '/:id'], chatMessageController.getAllChatMessages)
+router.get(
+    ['/', '/:id'],
+    checkAnyPermission('chatflows:view,chatflows:update,agentflows:view,agentflows:update'),
+    chatMessageController.getAllChatMessages
+)
 
 // UPDATE
+// [fork] no permission guard on abort: the whitelisted /api/v1/chatmessage/abort route is called by the public embed widget
 router.put(['/abort/', '/abort/:chatflowid/:chatid'], chatMessageController.abortChatMessage)
 
 // DELETE
-router.delete(['/', '/:id'], chatMessageController.removeAllChatMessages)
+router.delete(
+    ['/', '/:id'],
+    checkAnyPermission('chatflows:delete,agentflows:delete,assistants:delete'),
+    chatMessageController.removeAllChatMessages
+)
 
 export default router
